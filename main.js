ec5a1e3e87440eaf5fc1dc521c8c65d879b7e369
aramo2000 <aram_leblebjian@edu.aua.am>
2019-11-11 23:56:44 +0400

    Prime_Number

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-const util = require("./myUtils.js");
-
-console.log(util.isPrime(2));
+const reader = require('readline-sync')
+let n = reader.questionInt("Input the smallest number of your range = ")
+let m = reader.questionInt("Input the largest number of your range = ")
+while(m<n) {
+    console.log("It's a range, write it right")
+    n = reader.questionInt("Input the smallest number of your range = ")
+    m = reader.questionInt("Input the largest number of your range = ")
+}
+let primes = []
+for( let x=n ; x<=m ; x++) {
+    const util = require("./myUtils.js");
+    
+    if(util.isPrime(x)) {
+        primes.push(x)
+    }
+}
+console.log(primes)
 
--- myUtils.js
+++ myUtils.js
@@ -1,13 +1,13 @@
 module.exports = {
-	isPrime: function(n) {
-		if(n <=1) {
+	isPrime: function(x) {
+		if(x <=1) {
 			return false;
 		}
 
 		let div = 2;
 
-		while(div <= Math.sqrt(n)) {
-			if(n % div === 0) {
+		while(div <= Math.sqrt(x)) {
+			if(x % div === 0) {
 				return false;
 			}
 
